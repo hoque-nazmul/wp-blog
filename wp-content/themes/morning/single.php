b1e081f98a5b501df8abc942fede0968f32a88d1
nazmul98 <coder.nazmul@gmail.com>
2021-07-08 19:11:58 -0400

    pagination updated

---
+++ wp-content/themes/morning/single.php
@@ -0,0 +1,67 @@
+<?php 
+    get_header();
+?>
+<body <?php body_class(); ?>>
+<!-- Hero Section -->
+<?php get_template_part('template-parts/hero') ?>
+<div class="posts">
+    <?php if(have_posts()): ?>
+        <?php while(have_posts()): 
+           the_post(); 
+        ?>
+            <div <?php post_class(['post']); ?>>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a href="<?php the_permalink(); ?>">
+                                <h2 class="post-title">
+                                    <?php the_title(); ?>
+                                </h2>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <p>
+                                <strong>
+                                    <?php the_author(); ?>
+                                </strong>
+                                <br/>
+                                <?php get_the_date(); ?>
+                            </p>
+                            <?php 
+                                the_tags(
+                                    '<ul class="list-unstyled"><li>', 
+                                    '</li><li>', 
+                                    '</li></ul>'
+                                );
+                            ?>
+                        </div>
+                        <div class="col-md-8">
+                            <p>
+                                <?php 
+                                    if(has_post_thumbnail()) {
+                                        the_post_thumbnail('large', ['class' => 'img-fluid']);
+                                    }
+                                ?>
+                            </p>
+                            <?php 
+                                if (is_single()) {
+                                    the_content();
+                                } else {
+                                    the_excerpt();
+                                }
+                            ?>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+
+</div>
+<?php 
+    get_template_part('template-parts/pagination');
+    get_footer();
+?>
--- wp-content/themes/morning/template-parts/pagination.php
+++ wp-content/themes/morning/template-parts/pagination.php
@@ -1,12 +1,23 @@
 <div class="row mb-5">
     <div class="col-md-6 offset-md-3 bg-light py-3">
         <?php 
-            the_posts_pagination([
-                'mid_size' => 2,
-                'prev_text' => __('Previous Page', 'morning'),
-                'next_text' => __('Next Page', 'morning'),
-                'screen_reader_text' => __(' ')
-            ]);
+            if (is_single()) {
+            ?>
+                <div class="d-flex justify-content-between">
+                    <?php 
+                        previous_post_link();
+                        next_post_link();
+                    ?>
+                </div>
+            <?php 
+            } else {
+                the_posts_pagination([
+                    'mid_size' => 2,
+                    'prev_text' => __('Previous Page', 'morning'),
+                    'next_text' => __('Next Page', 'morning'),
+                    'screen_reader_text' => __(' ')
+                ]);
+            }
         ?>
     </div>
 </div>
