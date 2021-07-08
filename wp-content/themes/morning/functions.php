13154ec81c8014b6936a974a0ecfa097621c5b56
nazmul98 <coder.nazmul@gmail.com>
2021-07-07 20:40:35 -0400

    configured morning theme

---
+++ wp-content/themes/morning/functions.php
@@ -0,0 +1,19 @@
+<?php 
+die(wp_get_theme()->get('Version'));
+function morning_theme_setup () {
+    load_theme_textdomain('morning');
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'morning_theme_setup');
+
+function morning_theme_assets () {
+    // Cache Busting... 
+    if (site_url() === 'http://localhost/wp-blog') {
+        define('VERSION', time());
+    } else {
+        define('VERSION', wp_get_theme()->get('Version'));
+    }
+    wp_enqueue_style('main-css', get_stylesheet_uri(), null, VERSION);
+}
+add_action('wp_enqueue_scripts', 'morning_theme_assets');
