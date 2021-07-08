509fa2a879cd9f8d93f3cbbfae47e2ed484f423f
nazmul98 <coder.nazmul@gmail.com>
2021-07-08 18:21:56 -0400

    template-part used & all post displayed dynamically

--- wp-content/themes/morning/index.php
+++ wp-content/themes/morning/index.php
@@ -2,56 +2,64 @@
     get_header();
 ?>
 <body <?php body_class(); ?>>
-<div class="header">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h3 class="tagline">
-                    <?php bloginfo('description'); ?>
-                </h3>
-                <a href="<?php echo site_url(); ?>">
-                    <h1 class="align-self-center display-1 text-center heading">
-                        <?php bloginfo('name'); ?>
-                    </h1>
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
+<!-- Hero Section -->
+<?php get_template_part('template-parts/hero') ?>
 <div class="posts">
-    <div <?php post_class(['post']); ?>>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="post-title">This is a beautiful day in Dhaka!</h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <p>
-                        <strong>John Doe</strong><br/>
-                        15th May, 2018
-                    </p>
-                    <ul class="list-unstyled">
-                        <li>dhaka</li>
-                    </ul>
-                </div>
-                <div class="col-md-8">
-                    <p>
-                        <img class="img-fluid" src="https://images.pexels.com/photos/301929/pexels-photo-301929.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=800"
-                             alt="Post Title">
-                    </p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut deserunt dicta doloribus error,
-                        harum,
-                        illo impedit incidunt ipsam ipsum necessitatibus nihil perferendis perspiciatis provident quasi
-                        reiciendis saepe
-                        sequi sint, voluptatum?
-                    </p>
-                </div>
-            </div>
+    <?php if(have_posts()): ?>
+        <?php while(have_posts()): 
+           the_post(); 
+        ?>
+            <div <?php post_class(['post']); ?>>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a href="<?php the_permalink(); ?>">
+                                <h2 class="post-title">
+                                    <?php the_title(); ?>
+                                </h2>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <p>
+                                <strong>
+                                    <?php the_author(); ?>
+                                </strong>
+                                <br/>
+                                <?php get_the_date(); ?>
+                            </p>
+                            <?php 
+                                the_tags(
+                                    '<ul class="list-unstyled"><li>', 
+                                    '</li><li>', 
+                                    '</li></ul>'
+                                );
+                            ?>
+                        </div>
+                        <div class="col-md-8">
+                            <p>
+                                <?php 
+                                    if(has_post_thumbnail()) {
+                                        the_post_thumbnail('large', ['class' => 'img-fluid']);
+                                    }
+                                ?>
+                            </p>
+                            <?php 
+                                if (is_single()) {
+                                    the_content();
+                                } else {
+                                    the_excerpt();
+                                }
+                            ?>
+                        </div>
+                    </div>
 
-        </div>
-    </div>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+
 </div>
 <?php 
     get_footer();
---
+++ wp-content/themes/morning/template-parts/hero.php
@@ -0,0 +1,16 @@
+<div class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3 class="tagline">
+                    <?php bloginfo('description'); ?>
+                </h3>
+                <a href="<?php echo site_url(); ?>">
+                    <h1 class="align-self-center display-1 text-center heading">
+                        <?php bloginfo('name'); ?>
+                    </h1>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
