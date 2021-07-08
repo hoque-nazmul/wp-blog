b8d2aa0c47ddc1f6818dcfe8a4282d99faaa33ec
nazmul98 <coder.nazmul@gmail.com>
2021-07-08 19:43:01 -0400

    register two widgets

--- wp-content/themes/morning/functions.php
+++ wp-content/themes/morning/functions.php
@@ -28,3 +28,30 @@ function morning_theme_assets () {
     );
 }
 add_action('wp_enqueue_scripts', 'morning_theme_assets');
+
+function morning_widgets_init() {
+    register_sidebar(
+		array(
+			'name'          => __( 'Primary', 'morning' ),
+			'id'            => 'sidebar-1',
+			'description'   => __( 'Primary Sidebar', 'morning' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+
+    register_sidebar(
+		array(
+			'name'          => __( 'Secondary', 'morning' ),
+			'id'            => 'sidebar-2',
+			'description'   => __( 'Secondary Sidebar', 'morning' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
+}
+add_action('widgets_init', 'morning_widgets_init');
