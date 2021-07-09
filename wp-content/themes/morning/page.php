6b36f684926b9e7f24104b98f987e1afabb22d7f
nazmul98 <coder.nazmul@gmail.com>
2021-07-09 18:02:34 -0400

    hero section & page layout changed

--- wp-content/themes/morning/page.php
+++ wp-content/themes/morning/page.php
@@ -1,9 +1,5 @@
 <?php 
     get_header();
-    $default_width = 'col-md-9';
-    if(!is_active_sidebar('sidebar-1')) {
-        $default_width = 'col-md-10 offset-md-1';
-    }
 ?>
 <body <?php body_class(); ?>>
 <!-- Hero Section -->
@@ -11,7 +7,7 @@
 <div class="posts">
     <div class="container">
         <div class="row">
-            <div class="<?php echo $default_width; ?>">
+            <div class="col-md-10 offset-md-1">
                 <?php if(have_posts()): 
                     while(have_posts()): 
                     the_post(); 
@@ -19,31 +15,13 @@
                 <div <?php post_class(['post']); ?>>
                     <div class="row">
                         <div class="col-md-12">
-                            <a href="<?php the_permalink(); ?>">
-                                <h2 class="post-title text-center">
-                                    <?php the_title(); ?>
-                                </h2>
-                            </a>
+                            <h2 class="post-title text-center">
+                                <?php the_title(); ?>
+                            </h2>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-2">
-                            <p>
-                                <strong>
-                                    <?php the_author(); ?>
-                                </strong>
-                                <br/>
-                                <?php get_the_date(); ?>
-                            </p>
-                            <?php 
-                                the_tags(
-                                    '<ul class="list-unstyled"><li>', 
-                                    '</li><li>', 
-                                    '</li></ul>'
-                                );
-                            ?>
-                        </div>
-                        <div class="col-md-10">
+                        <div class="col-md-12">
                             <p>
                                 <?php 
                                     if(has_post_thumbnail()) {
@@ -60,15 +38,9 @@
                 <?php endwhile; ?>
             <?php endif; ?>
             </div>
-            <?php if(is_active_sidebar('sidebar-1')): ?>
-                <div class="col-md-3 justify-content-end">
-                    <?php dynamic_sidebar('sidebar-1'); ?>
-                </div>
-            <?php endif; ?>
         </div>
     </div>
 </div>
 <?php 
-    get_template_part('template-parts/pagination');
     get_footer();
 ?>
