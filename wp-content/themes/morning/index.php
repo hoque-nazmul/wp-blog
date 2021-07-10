d9f7653658558e121f725e45de4ef0632b882eef
nazmul98 <coder.nazmul@gmail.com>
2021-07-09 21:09:49 -0400

    added different sub-template using post format

--- wp-content/themes/morning/index.php
+++ wp-content/themes/morning/index.php
@@ -4,9 +4,6 @@
     if(!is_active_sidebar('sidebar-1')) {
         $default_width = 'col-md-10 offset-md-1';
     }
-    $videoIcon = '<span class="dashicons dashicons-video-alt3"></span>';
-    $imageIcon = '<span class="dashicons dashicons-format-image"></span>';
-    $quoteIcon = '<span class="dashicons dashicons-format-quote"></span>';
 ?>
 <body <?php body_class(); ?>>
 <!-- Hero Section -->
@@ -15,64 +12,13 @@
     <div class="container">
         <div class="row">
             <div class="<?php echo $default_width; ?>">
-                <?php if(have_posts()): 
-                    while(have_posts()): 
+                <?php if(have_posts()) {
+                    while(have_posts()) {
                     the_post(); 
+                        get_template_part('post-formats/content', get_post_format());
+                    }
+                }
                 ?>
-                <div <?php post_class(['post']); ?>>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <a href="<?php the_permalink(); ?>">
-                                <h2 class="post-title">
-                                    <?php the_title(); ?>
-                                </h2>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-3">
-                            <p>
-                                <strong>
-                                    <?php the_author(); ?>
-                                </strong>
-                                <br/>
-                                <?php echo get_the_date(); ?>
-                            </p> 
-                            <p class="font-size: 20px"><?php echo get_post_meta(get_the_ID(), 'place', true) ?></p>
-                            <?php 
-                           
-                                the_tags(
-                                    '<ul class="list-unstyled"><li>', 
-                                    '</li><li>', 
-                                    '</li></ul>'
-                                );
-                            ?>
-                            <?php 
-                                if (has_post_format('video')) {
-                                    echo $videoIcon;
-                                } elseif (has_post_format('image')) {
-                                    echo $imageIcon;
-                                } elseif (has_post_format('quote')) {
-                                    echo $quoteIcon;
-                                }
-                            ?>
-                        </div>
-                        <div class="col-md-9">
-                            <p>
-                                <?php 
-                                    if(has_post_thumbnail()) {
-                                        the_post_thumbnail('large', ['class' => 'img-fluid']);
-                                    }
-                                ?>
-                            </p>
-                            <?php 
-                                the_excerpt();
-                            ?>
-                        </div>
-                    </div>
-                </div>
-                <?php endwhile; ?>
-            <?php endif; ?>
             </div>
             <?php if(is_active_sidebar('sidebar-1')): ?>
                 <div class="col-md-3">
---
+++ wp-content/themes/morning/post-formats/content-image.php
@@ -0,0 +1,44 @@
+<div <?php post_class(['post']); ?>>
+    <div class="row">
+        <div class="col-md-12">
+            <a href="<?php the_permalink(); ?>">
+                <h2 class="post-title">
+                    <?php the_title(); ?>
+                </h2>
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-3">
+            <p>
+                <strong>
+                    <?php the_author(); ?>
+                </strong>
+                <br/>
+                <?php echo get_the_date(); ?>
+            </p> 
+            <p class="font-size: 20px"><?php echo get_post_meta(get_the_ID(), 'place', true) ?></p>
+            <?php 
+            
+                the_tags(
+                    '<ul class="list-unstyled"><li>', 
+                    '</li><li>', 
+                    '</li></ul>'
+                );
+            ?>
+            <span class="dashicons dashicons-format-image"></span>
+        </div>
+        <div class="col-md-9">
+            <p>
+                <?php 
+                    if(has_post_thumbnail()) {
+                        the_post_thumbnail('large', ['class' => 'img-fluid']);
+                    }
+                ?>
+            </p>
+            <?php 
+                the_excerpt();
+            ?>
+        </div>
+    </div>
+</div>
