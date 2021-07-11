b012caa64aed310a356f7ff23447952219764c12
nazmul98 <coder.nazmul@gmail.com>
2021-07-11 08:06:49 -0400

    editable search form added

--- wp-content/themes/morning/functions.php
+++ wp-content/themes/morning/functions.php
@@ -3,6 +3,7 @@ function morning_theme_setup () {
     load_theme_textdomain('morning');
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_theme_support( 'html5', array( 'search-form' ) );
     add_theme_support('post-formats', ['quote', 'video', 'audio', 'image']);
     register_nav_menu('primary', 'main-menu');
 }
